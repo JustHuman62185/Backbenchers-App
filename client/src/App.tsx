45922eabe276863f87818eb475997c5390359da4
JustHuman62185 <justhuman62185@gmail.com>
2025-05-30 18:33:24 +0530

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,40 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import BottomNavigation from "@/components/bottom-navigation";
+import Chatbot from "@/pages/chatbot";
+import ExcuseGeneratorPage from "@/pages/excuse-generator";
+import NotesMakerPage from "@/pages/notes-maker";
+import Leaderboard from "@/pages/leaderboard";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={NotesMakerPage} />
+        <Route path="/notes-maker" component={NotesMakerPage} />
+        <Route path="/excuse-generator" component={ExcuseGeneratorPage} />
+        <Route path="/community-chat" component={Chatbot} />
+        <Route path="/leaderboard" component={Leaderboard} />
+        <Route component={NotFound} />
+      </Switch>
+      <BottomNavigation />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
